b03a5e226266205958d7472d6b0f8d1af6d1952c
ekholil <kholilebrahim01@gmail.com>
2023-05-30 06:33:09 +0600

    husky lint stage set

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 const app: Application = express()
 
+app.use(cors())
+
+// parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
